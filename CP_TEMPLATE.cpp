bae3f95889e09149707ca08277d78f451955bf51
Rahul Ranjan <rahulxyad@gmail.com>
2024-08-22 00:52:47 +0530

    Create CP_TEMPLATE.cpp

---
+++ CP_TEMPLATE.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+const int MOD = 2e5 + 10;
+
+
+
+void solve()
+{
+
+     
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
